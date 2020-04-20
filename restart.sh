210c1f80cdd9f1fed8ff209fc2aa071013860be8
Charles Zhang <gzcharleszhang@hotmail.com>
2020-04-20 03:12:12 -0400

    updated script

--- restart.sh
+++ restart.sh
@@ -1,1 +1,1 @@
-docker build . -t dbot-image && docker stop dbot && docker rm dbot && docker run --name dbot -d dbot-image
+docker build . -t dbot-image && docker stop dbot && docker rm dbot && docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs dbot-image
