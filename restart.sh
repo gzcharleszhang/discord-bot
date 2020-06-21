c69b777815bfa1cce49b107a2f508353ff303941
Charles Zhang <gzcharleszhang@hotmail.com>
2020-06-21 18:50:33 -0400

    persist db

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 docker build . -t dbot-image && \
   docker stop dbot && \
   docker rm dbot && \
-  docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs dbot-image
+  docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs $(pwd)/db/discord-bot.db:/usr/src/app/dbb/discord-bot.db dbot-image
