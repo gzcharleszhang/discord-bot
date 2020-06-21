f4f5adcafa3d92c34409b5973977db3083e46a7c
Charles Zhang <gzcharleszhang@hotmail.com>
2020-06-21 18:54:20 -0400

    start

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker build . -t dbot-image && docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs $(pwd)/db/discord-bot.db:/usr/src/app/dbb/discord-bot.db dbot-image
+docker build . -t dbot-image && docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs -v $(pwd)/db/discord-bot.db:/usr/src/app/dbb/discord-bot.db dbot-image
