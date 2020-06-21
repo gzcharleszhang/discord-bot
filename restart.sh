1ab66dede570df2796fb32cc132c7ce11b13f843
Charles Zhang <gzcharleszhang@hotmail.com>
2020-06-21 18:53:27 -0400

    fix script

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 docker build . -t dbot-image && \
   docker stop dbot && \
   docker rm dbot && \
-  docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs $(pwd)/db/discord-bot.db:/usr/src/app/dbb/discord-bot.db dbot-image
+  docker run --name dbot -d -v $(pwd)/logs:/usr/src/app/logs \
+    -v :$(pwd)/db/discord-bot.db:/usr/src/app/dbb/discord-bot.db dbot-image
